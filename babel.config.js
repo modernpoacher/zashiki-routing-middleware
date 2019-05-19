d7807ce92be0366e586a38524a7ed181a4c311a0
Jonathan Perry <jonathanperry@modernpoacher.com>
2019-05-20 00:28:45 +0100

    `package.json`

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  compact: true,
+  comments: false,
+  presets: [
+    [
+      '@babel/env', {
+        useBuiltIns: 'entry',
+        targets: {
+          node: 'current',
+          browsers: [
+            'last 2 versions'
+          ]
+        },
+        corejs: '3.0.1'
+      }
+    ],
+    '@babel/react'
+  ]
+}
