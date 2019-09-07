7ead412e319c51a86348d4f3428c72156d8ee54c
Jonathan Perry <jonathanperry@sequencemedia.net>
2019-09-07 23:28:38 +0100

    Pantograph

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
 import {
-  browserHistory as history
-} from 'react-router'
-
-import {
   Pantograph
 } from 'shinkansen-pantograph'
 
-export {
-  history
-}
+export default (store) => {
+  const {
+    OMEGA: {
+      ROUTE: OMEGA_ROUTE
+    },
+    EMBARK: {
+      ROUTE: EMBARK_ROUTE
+    },
+    DEBARK: {
+      ROUTE: DEBARK_ROUTE
+    },
+    ALPHA: {
+      ROUTE: ALPHA_ROUTE
+    }
+  } = Pantograph
 
-export default function routingMiddleware (store) {
-  const last = (action) => (
-    Pantograph.graphite({ history, store, action })
-  )
-  return (next) => (action) => last(next(action)) || action
+  return (next) => (action) => {
+    const {
+      type
+    } = action
+
+    if (
+      type === OMEGA_ROUTE ||
+      type === EMBARK_ROUTE ||
+      type === DEBARK_ROUTE ||
+      type === ALPHA_ROUTE
+    ) {
+      const {
+        history
+      } = action
+
+      if (history) {
+        Pantograph.graphite({
+          history: {
+            push: (location) => history.push(location),
+            getCurrentLocation: () => history.location
+          },
+          state: store.getState(),
+          action
+        })
+      }
+    }
+    return next(action)
+  }
 }
